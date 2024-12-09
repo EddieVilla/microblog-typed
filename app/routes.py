a3359c12fff7cfc9f87f9006fb243ecc3afe9942
villasr2 <edward.villasenor@gmail.com>
2024-12-09 09:42:55 -0600

    first template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return "Hello"
+    user = {"username": "Miguel"}
+    return render_template("index.html", title="Home", user=user)
